afc2bb1b68d8d1654aea8a471b0ae699f0336894
pdbailey0 <pbailey@air.org>
2022-06-09 08:49:35 -0500

    add some documentation to ittyBitty, including use case

--- R/krep.R
+++ R/krep.R
@@ -12,7 +12,19 @@ setClass("ittyBitty",
   return(x)
 }
 
-#' make a new, clean ittyBitty
+#' make a new ittyBitty number
+#' 
+#' @k numeric, the exponent
+#' @w numeric, the mantista
+#' 
+#' An \code{ittyBitty} is a representation of a numer as w * 2^k
+#' 
+#' A simply way to cast a number (e.g., \code{myNum})as an \code{ittyBitty} is to use \code{ittyBitty(k=1,w=myNum)}, 
+#' The expected method of creating an \code{ittyBitty} is \code{exp_to_ittyBitty} which finds
+#' \code{exp} of \code{x} as an \code{ittyBitty}. These can then be added together for numerical quadrature
+#' and the results can be returned as a numeric with \code{log_as_numeric}. Doing this, far smaller
+#' values can be represented than with doubles.
+#' 
 #' @importFrom methods new
 #' @export
 ittyBitty <- function(k=double(), w=double()) {
@@ -108,84 +120,3 @@ setMethod("Arith", signature(e1="ittyBitty", e2="ittyBitty"), Arith.ittyBitty.it
 setMethod("Arith", signature(e1="ittyBitty", e2="numeric"), Arith.ittyBitty.numeric)
 setMethod("Arith", signature(e1="numeric", e2="ittyBitty"), Arith.numeric.ittyBitty )
 
-if(FALSE) {
-.log.ittyBitty(aa <- .mult.ittyBitty.numeric(exp_to_ittyBitty(-40), 2))
-.log.ittyBitty(aa <- exp_to_ittyBitty(-40) * 2)
-.log.ittyBitty(aa <- 2*exp_to_ittyBitty(-40))
-print.ittyBitty(aa)
-.log.ittyBitty(bb <-  exp_to_ittyBitty(-40) + exp_to_ittyBitty(-40))
-
-.log.ittyBitty(aa <- .mult.ittyBitty.ittyBitty(exp_to_ittyBitty(-40), exp_to_ittyBitty(-40)))
-
-
-.log.ittyBitty(aa <- .mult.ittyBitty.numeric(exp_to_ittyBitty(-900),200))
-900 + .log.ittyBitty( b <- .add.ittyBitty(a1 <- exp_to_ittyBitty(-900), a2 <- exp_to_ittyBitty(-910)))
-
-require(Rmpfr)
-900 + as.numeric(log(exp(mpfr(-900, 120)) + exp(mpfr(-910, 120))))
-
-900 + .log.ittyBitty( b <- .add.ittyBitty(a1 <- exp_to_ittyBitty(-900), .mult.ittyBitty.numeric(exp_to_ittyBitty(-910),250)))
-900 + as.numeric(log(exp(mpfr(-900, 120)) + 250*exp(mpfr(-910, 120))))
-
-require(microbenchmark)
-microbenchmark(
- .log.ittyBitty( b <- .add.ittyBitty(a1 <- exp_to_ittyBitty(-900), .mult.ittyBitty.numeric(exp_to_ittyBitty(-910),250))),
- as.numeric(log(exp(mpfr(-900, 32)) + 250*exp(mpfr(-910, 32))))
-)
-
-aseq <- -700 * 2^(-1:20)
-bseq <- c(10, 20, 50)
-K <- length(aseq) * length(bseq)
-df <- data.frame(a=rep(0, K),
-                 b=rep(0, K),
-                 double=rep(0, K),
-                 mpf120=rep(0, K),
-                 mpf240=rep(0, K),
-                 kr=rep(0, K),
-                 diff = rep(0, K))
-dfi <- 1
-for(i in aseq) {
-  for(j in bseq) {
-    raw <- log(exp(i) + 1250*exp(i-j))
-    kout <- .log.ittyBitty(.add.ittyBitty(exp_to_ittyBitty(i), .mult.ittyBitty.numeric(exp_to_ittyBitty(i-j),1250)))
-    mpfrout <- as.numeric(log(exp(mpfr(i, 120)) + 1250*exp(mpfr(i-j, 120))))
-    mpfrout240 <- as.numeric(log(exp(mpfr(i, 480)) + 1250*exp(mpfr(i-j, 480))))
-    df[dfi,] <- c(i, j, i-raw, i - mpfrout, i - mpfrout240,
-                  i - kout, NA)
-    dfi <- dfi + 1
-  }
-}
-df$true <- df$mpf240
-df$true[64:nrow(df)] <- rep(df$true[61:63], length(64:nrow(df))/3)
-df$diff <- (df$true - df$kr)/ pmax(abs(df$kr), .Machine$double.eps)
-df
-par(mfrow=c(3,3))
-with(subset(df, b==10), plot(range(-1*a),range(kr, mpf240), log="x", type="n"))
-with(subset(df, b==10), lines(-1*a,kr,col="blue"))
-with(subset(df, b==10), lines(-1*a,mpf240, col="red"))
-
-with(subset(df, b==20), plot(range(-1*a),range(kr, mpf240), log="x", type="n"))
-with(subset(df, b==20), lines(-1*a,kr,col="blue"))
-with(subset(df, b==20), lines(-1*a,mpf240, col="red"))
-
-with(subset(df, b==50), plot(range(-1*a),range(kr, mpf240), log="x", type="n"))
-with(subset(df, b==50), lines(-1*a,kr,col="blue"))
-with(subset(df, b==50), lines(-1*a,mpf240, col="red"))
-
-
-
-with(subset(df, b==10 & mpf240 < 100), plot(-1*a,kr-mpf240, log="x"))
-
-with(subset(df, b==20 & mpf240 < 100), plot(-1*a,kr-mpf240, log="x"))
-
-with(subset(df, b==50 & mpf240 < 100), plot(-1*a,kr-mpf240, log="x"))
-
-
-with(subset(df, b==10 & mpf240 < 100), plot(-1*a,(kr-mpf240)/mpf240, log="x"))
-
-with(subset(df, b==20 & mpf240 < 100), summary((kr-mpf240)/mpf240))
-with(subset(df, b==20 & mpf240 < 100), plot(-1*a,(kr-mpf240)/mpf240, log="x"))
-
-with(subset(df, b==50 & mpf240 < 100), summary((kr-mpf240)/mpf240))
-with(subset(df, b==50 & mpf240 < 100), plot(-1*a,(kr-mpf240)/mpf240, log="x"))
-}
